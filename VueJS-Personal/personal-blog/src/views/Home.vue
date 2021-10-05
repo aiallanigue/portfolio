cbd9d6778c0daaa461715c97f51df5403eed725a
Lan Allanigue <allanallanigue@gmail.com>
2021-10-05 23:19:39 +0800

    Food blog made with VueJS

---
+++ VueJS-Personal/personal-blog/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <span>
+        <hello-world></hello-world>
+        <home-details></home-details>
+        <home-plans></home-plans>
+    </span>
+</template>
+<script>
+import HelloWorld from '@/components/HelloWorld';
+import HomeDetails from '@/components/HomeDetails';
+import HomePlans from '@/components/HomePlans';
+
+export default {
+    name: 'Home',
+    components: {
+        HelloWorld,
+        HomeDetails,
+        HomePlans,
+    },
+};
+</script>
